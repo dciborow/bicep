b0b30d6a01a11c282e0608df6d8c57bba521d9e8
Daniel Ciborowski <dciborow@microsoft.com>
2023-01-05 21:05:25 -0500

    Update BicepFoldingHandler.cs

--- src/Bicep.LangServer/Handlers/BicepFoldingHandler.cs
+++ src/Bicep.LangServer/Handlers/BicepFoldingHandler.cs
@@ -13,44 +13,6 @@ namespace Bicep.LanguageServer.Handlers
 {
     public class BicepFoldingHandler : FoldingRangeHandlerBase
     {
-        private readonly ILogger<BicepFoldingHandler> logger;
-        private readonly ICompilationManager compilationManager;
-
-        // TODO: Not sure if this needs to be shared.
-        private readonly FoldingLegend legend = new();
-
-        public BicepFoldingHandler(ILogger<BicepFoldingHandler> logger, ICompilationManager compilationManager)
-        {
-            this.logger = logger;
-            this.compilationManager = compilationManager;
-        }
-
-        protected override Task<FoldingDocument> GetFoldingDocument(ITextDocumentIdentifierParams @params, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(new FoldingDocument(this.legend));
-        }
-
-        protected override Task Fold(FoldingBuilder builder, ITextDocumentIdentifierParams identifier, CancellationToken cancellationToken)
-        {
-            var compilationContext = this.compilationManager.GetCompilation(identifier.TextDocument.Uri);
-
-            if (compilationContext != null)
-            {
-                FoldingVisitor.BuildFolding(builder, compilationContext.Compilation.SourceFileGrouping.EntryPoint);
-            }
-
-            return Task.CompletedTask;
-        }
-
-        protected override FoldingRegistrationOptions CreateRegistrationOptions(FoldingCapability capability, ClientCapabilities clientCapabilities) => new()
-        {
-            DocumentSelector = DocumentSelectorFactory.Create(),
-            Legend = this.legend,
-            Full = new FoldingCapabilityRequestFull
-            {
-                Delta = true
-            },
-            Range = true
-        };
+        private readonly ILogger<BicepFoldingHandler> logger;       
     }
 }
